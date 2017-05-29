d68c43596919e76b2d058a2729fd347098c958f3
Pitt009 <gmartinez28837@gmail.com>
2017-05-29 16:20:15 -0500

    Falta funcion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,40 @@
+$(document).ready();
+
+
 $(function(){
     $('#slider div:gt(0)').hide();
     setInterval(function(){
       $('#slider div:first-child').fadeOut(0)
          .next('div').fadeIn(1000)
          .end().append('#slider');}, 4000);
+});
+
+$("#btn1").click(function () {
+  $("#btn1").css("background-color", "pink");
+  $("#imagen1").show();
+
+});
+
+$("#btn2").click(function () {
+  $("#btn2").css("background-color", "pink");
+  $("#imagen2").show();
+
+});
+
+$("#btn3").click(function () {
+  $("#btn3").css("background-color", "pink");
+  $("#imagen3").show();
+
+});
+
+$("#btn4").click(function () {
+  $("#btn4").css("background-color", "pink");
+  $("#imagen4").show();
+
+});
+
+$("#btn5").click(function () {
+  $("#btn5").css("background-color", "pink");
+  $("#imagen5").show();
+
 });
